b7ee05e6594b5216a0bb03c0b11f9c1884f9ff73
Chi <chulinguy@gmail.com>
2017-06-22 23:53:27 -0700

    working on fiftyfifty

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,21 +11,22 @@ app.timer = 0;
 app.intervalF = function(){}; 
 app.timeoutF = function(){};
 app.questionNumber = 0;
-app.questionsForThisGame = [];  
+app.questionsForThisGame = []; 
+app.LLusedThisRound = false;   
 //game settings
-app.introWaitTime = 5; 
-app.intermissionWaitTime = 5; 
-app.pauseTimeBetweenQuestions = 4; 
+app.introWaitTime = 2; 
+app.intermissionWaitTime = 2; 
+app.pauseTimeBetweenQuestions = 2; 
 app.difficultySetting = {
-  'Easy': 1,
-  'Medium': 1,
-  'Hard': 1
+  'Easy': 2,
+  'Medium': 2,
+  'Hard': 2
 };
 app.lifelines = ['phoneAFriend','pollTheAudience', 'fiftyFifty'];
 app.difficultyTimer = {
-  'Easy': 6,
-  'Medium': 6,
-  'Hard': 6
+  'Easy': 60,
+  'Medium': 60,
+  'Hard': 60
 }
 
 //logic for choosing questions for this round 
@@ -36,17 +37,27 @@ app.chooseQuestions = function () {
   //for each difficulty setting
   for (var key in that.difficultySetting) {
     //take all questions of that difficulty, shuffle it, and pick n out of the set, where n is the value in the difficultySetting key-value pair
-    Qs = Qs.concat(chance.pickset(chance.shuffle(_.filter(this.allQuestionsAnswers, (v) => (v.difficulty === key))), that.difficultySetting[key]));
+    Qs = Qs.concat(chance.pickset(chance.shuffle(_.filter(that.allQuestionsAnswers, (v) => (v.difficulty === key))), that.difficultySetting[key]));
   }
   //update game state: questions for this game 
   this.questionsForThisGame = Qs; 
 }
+//Logic for checking if user inputs a correct answer
+app.answerCheck = function (v) {
+  console.log(`user entered ${v}`)
+  if (v === this.answer) {
+    this.goodAnswer();
+  } else {
+    this.noGoodAnswer(v)
+  }
+}
 
-//logic for showing solution if user waits out a question or answers incorrectly
-app.noGoodAnswer = function(answered) {
-  console.log("display solution");
+//logic for all after-question actioms
+app.afterQuestion = function (){
+  console.log('processing all actions between questions')
+  var that = this;
+  $('#choices').empty();
   clearTimeout(this.timeoutF);
-  var that = this;  
   //logic to determine if we should trigger next question or trigger either intermission or gameover
   if (this.questionNumber === this.difficultySetting.Easy){
     this.timeoutF = setTimeout(that.intermission.bind(that), that.pauseTimeBetweenQuestions * 1000);
@@ -55,16 +66,35 @@ app.noGoodAnswer = function(answered) {
   } else {
     this.timeoutF = setTimeout(that.nextQuestion.bind(that), that.pauseTimeBetweenQuestions * 1000);
   }
+  this.timer = that.pauseTimeBetweenQuestions;
+  $('#timeLeft').html(that.pauseTimeBetweenQuestions);
+}
+
+//Logic for good answer
+app.goodAnswer = function () {
+  console.log("good answer!");
+  this.afterQuestion();
+  //update game state
+  this.rights++;
+  //congrates the user! 
+  $('#question').text(`Good job! ${this.answer} is the correct answer.`);
+}
+
+//logic for showing solution if user waits out a question or answers incorrectly
+app.noGoodAnswer = function(answered) {
+  console.log("display solution due to wrong choice or no choice");
+  this.afterQuestion();
+  var filler = ""; 
   //update game states
   if (answered) {
     this.wrongs++;
+    filler = 'Wrong answer!'
   } else {
     this.omits++; 
+    filler = "Time's up!"
   }
-  this.timer = that.pauseTimeBetweenQuestions;
-  //update onscreen displays
-  $('#question').text(`The correct answer is ${this.answer}`);
-  $('#timeLeft').html(that.pauseTimeBetweenQuestions);
+  //show the correct answer
+  $('#question').text(`${filler}  The correct answer is ${this.answer}`);
 }
 
 //logic for nextQuestion method 
@@ -78,6 +108,7 @@ app.nextQuestion = function () {
   this.difficulty = this.questionsForThisGame[this.questionNumber].difficulty;
   this.questionNumber++; 
   this.timer = this.difficultyTimer[this.difficulty];
+  this.LLusedThisRound = false; 
   //render updated states
   this.render();
   //if user times out, move to noGoodAnswer logic
@@ -88,17 +119,28 @@ app.nextQuestion = function () {
 app.render = function () {
   console.log("rendering question info to DOM");
   var that = this; 
+  var multipleChoices = ['A', 'B', 'C', 'D']
   $('#timeLeft').text(that.difficultyTimer[that.difficulty]);
   $('#QID').text(that.questionNumber);
   $('#Qdiff').text(that.difficulty);
-  $('#question').text(that.question);
-  //TODO: render buttons
+  $('#question').html(that.question);
+  $('#IMGcontainer').html('<img id="regis" src="assets/images/regis.jpg">');
+  //logic to render the multiple choice buttons
+  $('#choices').empty();
+  multipleChoices.forEach((v, i) => {
+    var MCbutton = $('<button class="btn-primary col-xs-6">').text(`${v}.  ${that.choices[i]}`);
+    MCbutton.attr('data', that.choices[i])
+    MCbutton.on('click', function() {
+     that.answerCheck($(this).attr('data'))
+    })  
+    $('#choices').append(MCbutton);
+  })
 }
 //intermission logic
 app.intermission = function(){
   console.log('intermission time!');
   var that = this; 
-  $('#question').text("Now the game gets a lot tougher!  You have ${that.difficultyTimer.Medium} seconds to answer medium questions and ${that.difficultyTimer.Hard} seconds to answer hard questions. I hope you\'ve been saving your lifelines...")
+  $('#question').text(`Now the game gets a lot tougher!  You have ${that.difficultyTimer.Medium} seconds to answer medium questions and ${that.difficultyTimer.Hard} seconds to answer hard questions. I hope you\'ve been saving your lifelines...`)
    this.timeoutF = setTimeout(that.nextQuestion.bind(that), that.intermissionWaitTime * 1000);
    this.timer = that.intermissionWaitTime;
   $('#timeLeft').html(that.intermissionWaitTime);
@@ -112,7 +154,9 @@ app.gameOver = function(){
   $('#timeLeft').text('');
   $('#QID').text('');
   $('#Qdiff').text('');
-  $('#question').text('Game over!');
+  $('#question').html(`Game over! You answered:\n\n ${this.rights} questions correctly\n${this.wrongs} questions incorrectly\n${this.omits} questions were not answered`);
+  $('#choices').empty();
+  $('#startButton').toggle();
 }
 
 //logic to update on screen count-down
@@ -146,12 +190,12 @@ app.initialize = function (){
 app.initRender = function (time) {
   console.log('rendering initial message and images')
   var that = this;  
+  var regisHeight;
   //hide start button
   $('#startButton').toggle();
-  //hide all choice buttons
-  $('.choices').hide();
   //render host image
   $('#IMGcontainer').html('<img id="regis" src="assets/images/regis.jpg">');
+  $('#LLcontainer').empty(); 
   //create and render lifelines
   _.each(this.lifelines, (v) => {
     //logic for creating lifeline divs
@@ -164,19 +208,31 @@ app.initRender = function (time) {
     LLdiv.append(LLimg);
     //attach click event listener
     LLdiv.on('click', function() {
-      $(this).addClass('hidden');
-      that[$(this).attr('data')]();
+      if(!that.LLusedThisRound){
+        $(this).addClass('hidden');
+        that[$(this).attr('data')]();
+      }
     })
     //add it to DOM
     $('#LLcontainer').append(LLdiv);
   })
+  //update background
   //update timer
   $('#timeLeft').html(time);
   //update the question text
-  $('#question').text(`Let the game begin! You have ${that.difficultyTimer.Easy} seconds to answer each easy question!  Note that you have three lifelines: Phone a Friend, Poll the Audience, and Fifty-fifty.  You can only use one lifeline per game because Chi was too lazy to figure out the extra logic.`)
+  $('#question').text(`Let the game begin! You have ${that.difficultyTimer.Easy} seconds to answer each easy question!\nNote that you have three lifelines: Phone a Friend, Poll the Audience, and Fifty-fifty.\nYou can only use one lifeline per question.`)
+  
+  setTimeout(()=>{
+    regisHeight = $('#regisRow').height() + $('#GSH').height() + $('#LLcontainer').height();
+    $('#game').css({'background-color': 'lightblue', 'min-height': regisHeight});
+    console.log('is this working?')
+  }, 50)
 }
-
+//event listener for start button
 $('#startButton').on('click', () => (app.initialize()));
+
+
+ 
 
 //all the questions and answers for the game
 app.allQuestionsAnswers = [
@@ -201,7 +257,7 @@ app.allQuestionsAnswers = [
     "difficulty": "Easy"},
 
   {"question":"Websites sometimes send small pieces of data to be stored on the user's computer.  What are they called?",
-    "choices":["Cookies", "California rolls", "Cheerios", "Choicken of the Sea"],
+    "choices":["Cookies", "California rolls", "Cheerios", "Chicken of the Sea"],
     "answer":"Cookies",
     "difficulty": "Easy"},
 
@@ -220,6 +276,11 @@ app.allQuestionsAnswers = [
     "answer":"Spam",
     "difficulty": "Easy"}, 
 
+  {"question":"In Javascript, which of the following operator is not a comparison operator?",
+    "choices": ["=>", "!==", "===", "<="], 
+    "answer":"=>",
+    "difficulty": "Medium"}, 
+  
   {"question":"Which Javascript Function method CANNOT be used to manually set context for 'this'?",
     "choices": [".bind", ".call", ".apply", ".toSource"], 
     "answer":".toSource",
@@ -240,18 +301,23 @@ app.allQuestionsAnswers = [
     "answer":"Ember.js",
     "difficulty": "Medium"}, 
 
+  {"question":"Which of the following HTML tags is NOT self-closing by nature?", 
+    "choices":["<img>", "<br>", "<area>", "<audio>"], 
+    "answer":"<audio>",
+    "difficulty": "Medium"}, 
+
   //hard
   {"question":"In Javascript, which of the following CANNOT be considered an object under any circumstances?",
-    "choices":["Null", "NaN", "Function", "Array"], 
+    "choices":["Null", "NaN", "A function", "An array"], 
     "answer":"NaN", 
     "difficulty": "Hard"}, 
 
-  {"question":`What will the following code output to the console?\nvar a =5;\nvar b=5;\n(function(){\nvar a = b = 3;\n}\n\nconsole.log('a = ', a, ', b = ', b)`,
+  {"question":`What will the following code output to the console?<pre><code>var a =5;\nvar b=5;\n(function(){\n  var a = b = 3;\n}\n\nconsole.log('a = ', a, ', b = ', b)</pre></code>`,
     "choices":["a = 3, b = 3", "a = 5, b = 3", "a = 5, b = 5", "a = 3, b = 5"], 
     "answer":"a = 5, b = 3",
     "difficulty": "Hard"}, 
 
-  {"question":`What will the following code output to the console?\nvar a = 1\nfunction b() {\na = 10;\nreturn;\nfunction a() {}\n}\nb();\nconsole.log(a)`,
+  {"question":`What will the following code output to the console?<pre><code>var a = 1\nfunction b() {\n  a = 10;\n  return;\n  function a() {}\n}\nb();\nconsole.log(a)</pre></code>`,
     "choices":["1", "undefined", "10", "function (){}"],
     "answer":"1",
     "difficulty": "Hard"}, 
@@ -262,73 +328,96 @@ app.allQuestionsAnswers = [
     "difficulty": "Hard"}
 ]
 
-//EXTRA STUFF
+
+// EXTRA STUFF
 app.fiftyFifty = function () {
   console.log('invoking fiftyFifty')
+  var that = this;  
+  this.LLusedThisRound = true;
+  var copyArr = this.choices.slice();
+  copyArr.splice((copyArr.indexOf(that.answer)),1);
+  copyArr.splice(Math.floor(Math.random() *3),1);
+  _.each(copyArr, function(v){
+    console.log($('.btn-primary').attr('data'))
+  })
+    
+  
 }
 
-//logic for "Phone a friend" life line
+// logic for "Phone a friend" life line
 app.phoneAFriend = function(){
   console.log('invoking phoneAFriend')
-  // var sentences = {};
-  // var friendGuess; 
-  // //Confidence-related logic, to randomize what the friend would say  
-  //   //confidence levels 
-  //   var confidenceArr = ['high', 'mid', 'low'];
-  //   //randomly assigns a confidence level to the "friend"
-  //   var friendConfidence = confidenceArr[Math.floor(Math.random() * 3)];
-  //   //for high confidence, the friend would always give the correct answer 
-  //   if (friendConfidence === 'high') {
-  //     friendGuess = this.correctAnswer; 
-  //   //for mid confidence, the friend gives the correct answer 70% of the time for 4 choices
-  //   } else if (friendConfidence === 'mid') {
-  //     var midConfidenceRandom = Math.random(); 
-  //     if (midConfidenceRandom < 0.6) {
-  //       friendGuess = this.correctAnswer;
-  //     } else {
-  //       friendGuess = this.choices[_.random(0,3)];
-  //     }
-  //   //for low confidence, the friend gives the correct answer 25% of the time for 4 choices
-  //   } else {
-  //       friendGuess = this.choices[_.random(0,3)];
-  //   }
-  //   sentences.high = `The answer is ${friendGuess}, final answer.`;
-  //   sentences.mid = `I just read about this, let's see; I believe the answer is ${friendGuess}.`;  
-  //   sentences.low = `uhh, I would guess the answer is ${friendGuess}, but I really don't know.`;
-  // //Logic for choosing a specific friend & voice
-  //   //array of names and associated voices
-  //   var voices = [["UK English Female", "Isabelle"], ["UK English Male", "Archie"],["US English Female", "Carol Ann"],["US English Male", "Billy"]];
-  //   //randomly choose a friend 
-  //   var friendProps = voices[Math.floor(Math.random() * voices.length)];
-  //   var friendName = friendProps[1];
-  //   var friendVoice = friendProps[0];
-  // //TODO: rendering logic
-  // console.log(`Using the 'Phone A Friend Lifeline' to call ${friendName}...`)
-  // //responsiveVoice specific logic
-  // responsiveVoice.speak(sentences[friendConfidence], friendVoice, {onend: app.timerResume});
+  this.LLusedThisRound = true;
+  var sentences = {};
+  var friendGuess; 
+  //Confidence-related logic, to randomize what the friend would say  
+    //confidence levels 
+    var confidenceArr = ['high', 'mid', 'low'];
+    //randomly assigns a confidence level to the "friend"
+    var friendConfidence = confidenceArr[Math.floor(Math.random() * 3)];
+    //for high confidence, the friend would always give the correct answer 
+    if (friendConfidence === 'high') {
+      friendGuess = this.answer; 
+    //for mid confidence, the friend gives the correct answer 70% of the time for 4 choices
+    } else if (friendConfidence === 'mid') {
+      var midConfidenceRandom = Math.random(); 
+      if (midConfidenceRandom < 0.6) {
+        friendGuess = this.answer;
+      } else {
+        friendGuess = this.choices[_.random(0,3)];
+      }
+    //for low confidence, the friend gives the correct answer 25% of the time for 4 choices
+    } else {
+        friendGuess = this.choices[_.random(0,3)];
+    }
+    sentences.high = `The answer is ${friendGuess}, final answer.`;
+    sentences.mid = `I just read about this, let's see; I believe the answer is ${friendGuess}.`;  
+    sentences.low = `uhh, I would guess the answer is ${friendGuess}, but I really don't know.`;
+  //Logic for choosing a specific friend & voice
+    //array of names and associated voices
+    var voices = [["UK English Female", "Isabelle"], ["UK English Male", "Archie"],["US English Female", "Carol Ann"],["US English Male", "Billy"]];
+    //randomly choose a friend 
+    var friendProps = voices[Math.floor(Math.random() * voices.length)];
+    var friendName = friendProps[1];
+    var friendVoice = friendProps[0];
+  //TODO: rendering logic
+  console.log(`Using the 'Phone A Friend Lifeline' to call ${friendName}...`)
+  //responsiveVoice specific logic
+  responsiveVoice.speak(sentences[friendConfidence], friendVoice);
 
 }
 
-//logic for 'Poll The Audience' life line
+// logic for 'Poll The Audience' life line
 app.pollTheAudience = function (){
   console.log('invoking pollTheAudience')
-  console.log(Chart)
+  var that = this;  
+  this.LLusedThisRound = true;  
   //logic for swapping Regis picture with the bar chart 
   $('#IMGcontainer').empty();
   var canvas = $('<canvas>');
   canvas.attr('id', 'pollChart');
   $('#IMGcontainer').append(canvas);
-  var pollDataArr; 
-  //logic to generate random vote numbers for poll data 
+  var pollDataArr = [];
+  // logic to generate random vote numbers for poll data 
+  var leftOver = 80;
+  _.each(this.choices, function (v) {
+    var votes = 0; 
+    var rand = Math.floor(Math.random()*leftOver);
+    if (v === that.answer){
+      votes += 20; 
+    }
+    votes += rand;  
+    leftOver -=rand; 
+    pollDataArr.push(votes)
+  })
 
-
-  //chart.js logic
+  // chart.js logic
   var ctx = document.getElementById("pollChart").getContext('2d');
   var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
           //red, blue, yellow, green
-          labels: this.choices,
+          labels: ['A', 'B', 'C','D'],
           datasets: [{
               label: '# of Votes',
               data: pollDataArr,
